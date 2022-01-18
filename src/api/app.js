7bd7fe8ac1edbe47ce5817cb974d6c8c78edab7d
Layo Kaminski <layokaminski@gmail.com>
2022-01-18 20:20:46 -0300

    feat: Configuração inicial da API
    Descrição:
    Iniciando as configurações inicias da API para podermos iniciar com o
    desenvolvimento dela.

---
+++ src/api/app.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+module.exports = app;
